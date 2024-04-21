bae5dd06ad1078432b079e1cf9ed81d411c6823f
hugooSoul <mandujano@Hugos-MacBook-Pro.local>
2024-04-21 13:26:38 -0600

    Styling the header & using Clerk for authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        dd
+      </section>
+    </>
+  );
+}
